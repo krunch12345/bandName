03a0347458752e9ee85e84897bf6d78be86be8f2
Admin <admin@Admins-MacBook-Pro.local>
2020-10-29 01:23:42 +0300

    gotovo

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict'
 
-let bands = ['Группа-Один', 'ГРуппа два', 'Группа?Три', 'the BEST band']
+let bands = ['Группа-Один', 'ГРуппа+два', 'Группа?Три', 'the BEST, band']
 
 let bandsChange = bands.map(function (item) {
-  return `#${item}`
-    .replace(/\W+/g, ' ')
+  return `# ${item}`
+    .replace(/[&%()=*\-+?]/g, ` `)
+    .replace(/[\,]/g, ``)
     .toLowerCase()
     .split(' ')
     .map((word, index) => word[0].toUpperCase() + word.slice(1))
